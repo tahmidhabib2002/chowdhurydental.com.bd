15fda5c355fa8df764d2a69e453c9cb1ed5e62c7
Tahmid Habib Saad <tahmidhabib29@gmail.com>
2026-02-28 02:35:13 +0600

    Implement service worker registration in config.js
    Added service worker registration to config.js.

--- saad/config.js
+++ saad/config.js
@@ -4,3 +4,10 @@ const API_CONFIG = {
     apikey: "e7b0da5a67ded72bd064e59bc2c382fc9b946900",
     callerID: "1234"
 };
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/saad/sw.js')
+      .then(reg => console.log('Service Worker Registered!', reg))
+      .catch(err => console.log('Service Worker Registration Failed!', err));
+  });
+}
